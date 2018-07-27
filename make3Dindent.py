a71529eb9ccda3b2b120e0282b22711da3cdac63
nao0x2c6 <geeklyfrog@gmail.com>
2018-07-28 01:17:49 +0900

    fix

--- make3Dindent.py
+++ make3Dindent.py
@@ -7,12 +7,14 @@ end = '}' # ネストの終点
 width = 20 # 改行幅
 
 def make3Dindent(stream, reverse=False):
-  depth = 0
-  charcount = 0
-  left = ''
-  right = ' ' * depth # いまの浮かび具合（右側のテキストに適用）
-  charcount = 0
+  depth = 0 # いまの浮かび具合（右側のテキストに適用）
+  depthList = [] # 行ごとの浮かび具合の管理
+  leftLines = []
+  rightLines = []
   for line in stream: 
+    charcount = 0
+    left = ''
+    right = ''
     for c in line:
       charcount += 1
       if(c == start):
@@ -25,35 +27,50 @@ def make3Dindent(stream, reverse=False):
         depth -= 1
         left += c + ' '
         right += c
+      elif charcount >= width or c == "\n": # 改行処理
+        pad = (width - charcount) * ' '
+        leftLines.append(left + pad)
+        rightLines.append(right + pad)
+        depthList.append(depth)
+        left = ''
+        right = ''
+        charcount = 0
+        continue
       else:
         left += c
         right += c
 
-      if charcount >= width or c == "\n": # 改行処理
-        left += "\n"
-        right += "\n" + (' ' * depth)
-        charcount = 0
-
   if charcount > 0:
-    left += "\n"
-    right += "\n"
+    leftLines.append(left)
+    rightLines.append(right)
+    depthList.append(depth)
+
+  maxDepth = max(depthList)
+
+  retRight = []
+  retLeft = []
+
+  for (l, r, d) in zip(leftLines, rightLines, depthList):
+    retLeft.append((' ' * (maxDepth - d)) + l)
+    retRight.append((' ' * d) + r)
 
   # 焦点補助のドット
-  dot = ' ' * int(width / 2) + '●'
-  left += dot
-  right += dot
+  pad = ' ' * int(width / 2)
+  dotLine = pad + '●' + pad
+  retLeft.append(dotLine)
+  retRight.append(dotLine)
 
   if reverse:
     # 沈む
-    return (right, left)
+    return (retRight, retLeft)
   else:
     # 浮かぶ
-    return (left, right)
+    return (retLeft, retRight)
 
 # 出力用
 def printLeftRight(left, right):
-  for (l, r) in zip(left.split("\n"), right.split("\n")):
-    print (l + ((width - len(l)) * ' ') + '    ' + r)
+  for (l, r) in zip(left, right):
+    print (l + '    ' + r)
 
 
 if __name__ == '__main__':
